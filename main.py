5547f6e18aba45993c7fa0e94c17ac1a361e198e
Bak Daulet Abzhatov <bakdauletabzhatov@MacBook-Pro-Bak.local>
2026-05-18 12:42:42 +0500

    connect google sheets prices

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import csv
+import urllib.request
+from io import StringIO
 
 app = Flask(__name__)
 CORS(app)
+
+SHEET_ID = "1IVvCwJdkxtTNpWh8NKWloczlwuuqbyDcufe7uP9QAVM"
+SHEET_URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv"
 
 @app.route("/")
 def home():
@@ -10,13 +16,31 @@ def home():
 
 @app.route("/price")
 def price():
-    product = request.args.get("product", "NO PRODUCT")
+    query = request.args.get("product", "").lower()
+
+    response = urllib.request.urlopen(SHEET_URL)
+    data = response.read().decode("utf-8")
+    reader = csv.DictReader(StringIO(data))
+
+    for row in reader:
+        product_name = row.get("Товар", "").lower()
+
+        if query in product_name:
+            return jsonify({
+                "product": row.get("Товар", ""),
+                "cash_price": row.get("Цена нал", ""),
+                "kaspi_price": row.get("Наша цена Kaspi", ""),
+                "stock": row.get("КОЛВО", ""),
+                "pspdf_link": row.get("Ссылка pspdf", ""),
+                "kaspi_link": row.get("Ссылка Kaspi", "")
+            })
 
     return jsonify({
-        "product": product,
-        "cash_price": "900 000 ₸",
-        "kaspi_price": "950 000 ₸"
+        "product": query,
+        "cash_price": "Не найдено",
+        "kaspi_price": "Не найдено",
+        "stock": "0"
     })
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=10000)
